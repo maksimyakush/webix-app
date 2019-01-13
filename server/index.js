6304f581829082e12fadf12e4ca53ef789ca8ad7
maksimyakush <maksimo-1988@yandex.ru>
2019-01-13 19:06:58 +0300

    feat(task-3): config heroku app

--- server/index.js
+++ server/index.js
@@ -7,8 +7,7 @@ const app = express();
 
 app.use(express.static("./src"));
 
-
 app.get("/data", async (req, res) => res.send(data));
 app.get("/products", async (req, res) => res.send(products));
 app.get("/users", async (req, res) => res.send(users));
-app.listen(3000, () => console.log("listen"));
+app.listen(process.env.PORT);
